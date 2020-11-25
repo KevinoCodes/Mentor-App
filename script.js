ae2877a1f3f5e589c7681cc7a8ed6209a3e91164
KevinoCodes <58299737+KevinoCodes@users.noreply.github.com>
2020-11-25 01:20:20 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function UserScroll() {
+    for (let i = 0; i < 3; i++) {
+        document.getElementById("UserScroll1").innerHTML += "<div id='UserScroll1' class='container'><div class='section'><div class='row'><div class='col xl3'><div class='card'><div class='card-image'><img src='Placeholder.png'><span class='card-title black-text'>Card Title</span><a class='btn-floating halfway-fab waves-effect waves-light red'><i class='material-icons'>add</i></a></div><div class='card-content'><p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p></div></div></div>"
+    
+    }
+}
