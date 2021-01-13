2cb2d96d5f1140530576b3c08754729b19a7ca5c
Rohin <me@r0h.in>
2021-01-13 16:56:16 -0500

    Landing Page & Navbar
    Co-Authored-By: KevinoCodes <58299737+KevinoCodes@users.noreply.github.com>

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,57 @@
-import { Container } from '@material-ui/core';
+import { Container, AppBar, Toolbar, IconButton, Button, Typography, Paper } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
 import './App.css';
-import TopNav from './components/TopNav';
+import { createMuiTheme } from '@material-ui/core/styles';
+import grey from '@material-ui/core/colors/grey';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: grey[500],
+    },
+    secondary: {
+      main: grey[500],
+    },
+  },
+});
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 
 function App() {
-  return (
-    <div>
-      <TopNav />
-      <Container>
+  const classes = useStyles();
 
+  return (
+    <ThemeProvider theme={theme}>
+      <br />      <br />      <br />      <br />
+      <Container className="heroImage">
+        <AppBar className="navBar" position="static">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+        <br /><br /><br /><br /><br /><br /><br /><br /><br />
       </Container>
-    </div>
+      <Paper className="footer" elevation={0}></Paper>
+    </ThemeProvider>
   )
 }
 
